e91ea19daf8778177d492376038ef0a0db207520
Moon-999 <munhui3078@naver.com>
2022-11-30 17:24:00 +0900

    updated

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 # -*- coding: utf-8 -*-
 
+#core pkgs
 import streamlit as st
+
+#data pkgs
+import pandas as pd
+
+#이미지 라이브러리
+from PIL import Image
 
 def main():
     """코드작성"""
-    st.title("HELLO World!")
-    st.subheader("임시...")
+    # 텍스트 Input
+    fname = st.text_input('이름 입력')
+    st.write(fname)
 
-    # text
-    num = 1
-    st.text(f'숫자는 {num}')
+    #텍스트 Area
+    message = st.text_area('입력해주세용!', height=100)
+    st.write(message)
 
-    # markdown
-    st.markdown('## 마크다운 제목 2번째')
+    # 숫자 입력
+    number = st.number_input('숫자 입력')
+    st.write(number)
 
-    # 색상
-    st.success("성공")
-    st.warning("경고")
-    st.info("정보")
-    st.error("에러")
-    st.exception("예외")
+    # 날짜
+    myDate = st.date_input('날짜')
+    st.write(myDate)
+
+    #시간
+    myTime = st.time_input('시간')
+    st.write(myTime)
+
+    #Color Picker
+    color = st.color_picker('색상 선택')
+    st.write(color)
 
 if __name__=="__main__":
     main()
